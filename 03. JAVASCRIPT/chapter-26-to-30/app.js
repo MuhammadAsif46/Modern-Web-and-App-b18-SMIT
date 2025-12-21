14bf81fc1be0349bbbb318241ad5de10ab1d3163
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-21 11:30:18 +0500

    today class code update

---
+++ 03. JAVASCRIPT/chapter-26-to-30/app.js
@@ -0,0 +1,70 @@
+// console.log("hello");
+
+// ReplaceAll
+
+// var text = "In previous chapters you learned two different ways to replace World War II with the Second World War in a string. First,"
+// console.log(text.replaceAll("War","Jung"));
+
+
+// Math method:
+// round()
+// ceil()
+// floor()
+// random()
+// parseInt()
+// parseFloat()
+// Number()
+// toString()
+
+// var num = 2.5
+// console.log(Math.round(num));
+
+
+// var num = Math.random() * 100;
+// console.log(Math.floor(num));
+
+// var bigDecimal = Math.random();
+// var improvedNum = (bigDeci+mal * 6) + 1;
+// var numberOfStars = Math.floor(improvedNum);
+// console.log(numberOfStars);
+
+
+// +"77.74273"
+// 77.74273
+// parseInt("77.74273")
+// 77
+// parseFloat("1.9999")
+// 1.9999
+
+
+// Number("4563")
+// 4563
+// Number("4563.46756734")
+// 4563.46756734
+
+
+
+// var num = 5643
+// console.log(num);
+// console.log(typeof num.toString() + "-->" + num.toString());
+
+
+// 45.465234542368.toFixed()
+// '45'
+// 45.465234542368.toFixed(2)
+// '45.47'
+// 45.465234542368.toFixed(4)
+// '45.4652'
+
+// var num = 34.4765354678
+// var res = num.toFixed(3)
+// console.log(Number(res));
+
+
+
+// chapter: 21
+
+// chapter: 22
+ 
+
+
