c52147b236ae34bf5c0552af240e3c63c8c125e0
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-26 11:45:13 +0500

    today class code Date chapters

---
+++ 03. JAVASCRIPT/chapter-31-to-34/app.js
@@ -0,0 +1,28 @@
+// console.log("hello");
+
+
+// Chapter : 31 : Date:
+
+// var todayDate = new Date();
+// // console.log(todayDate);
+// var days = ['sun', 'mon', 'tue', 'wed','thr', 'fri', 'sat']
+// console.log(days[todayDate.getDay()]);
+
+
+// new Date().getTime()
+
+// var firstRamadan = new Date("Feb 18, 2026")
+// var today = new Date()
+
+// var firstRamadanGetTime = firstRamadan.getTime()
+// var todayGetTime = today.getTime()
+// var finalValue = firstRamadanGetTime - todayGetTime
+// var lastAnswer = Math.floor(finalValue / (1000 * 60 * 60 * 24));
+// console.log("ramadan -->",firstRamadanGetTime);
+// console.log("today -->",todayGetTime);
+// console.log("final -->", lastAnswer);
+// document.write("<h1>" + lastAnswer + " Days Left Arrival of 1st Ramadan 2026")
+
+
+// your age is : 21
+// your date of birth year is : 2004 
