8b15a2984a55c90ea72da95eeef319b77520a0b2
muhammadasif46 <asifahmed32c@gmail.com>
2026-01-09 11:23:49 +0500

    add today class code

---
+++ 03. JAVASCRIPT/chapter-40-to-47/app.js
@@ -0,0 +1,28 @@
+// console.log("hello");
+
+// for (var i = 0; i < 10; i++) {
+//    console.log(i);  
+// }
+
+// var i = 0;
+// while(i < 5){
+//     console.log(i);
+//     i++
+// }
+
+
+// var i = 0;
+/* do {
+    console.log(i);
+    i++
+} while(i > 5) */
+
+
+function greet(){
+    console.log("Welcome!");
+}
+
+function sum(a , b){
+    console.log(a + b);
+}
+// sum()
