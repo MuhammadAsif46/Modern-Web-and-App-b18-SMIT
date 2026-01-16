537852b579f53b3c0303f13d70c7ba0f30754e45
muhammadasif46 <asifahmed32c@gmail.com>
2026-01-16 11:27:21 +0500

    today class code

---
+++ 03. JAVASCRIPT/chapter-48-to-52/app.js
@@ -0,0 +1,24 @@
+// function getInputValue() {
+//     var input = document.getElementById("username")
+//     var inputValue = input.value
+//     console.log(inputValue.trim());
+//     // if (input.value == trim()) {
+//     //     alert("Please Enter correct value")
+//     // } else {
+//     //     console.log(input.value);
+//     // }
+// }
+
+
+// function getInputValue() {
+//     var city = "karachi"
+//     var input = document.getElementById("username").value = city
+//     console.log(input);
+// }
+
+function expand(){
+    var moreValue = `Slow lorises are a group of several species of trepsirrhine primates whichmake up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety ofdistinctive colortionpatterns that are species-dependent. The hands and feet of slow lorises have several
+adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+Slow lorises have a toxic bite, a rare trait among mammals.`
+    document.getElementById("para").innerHTML = moreValue
+}
