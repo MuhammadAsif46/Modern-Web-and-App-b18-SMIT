fc21e8a348541572770ed526cdb33c7bdb883215
muhammadasif46 <asifahmed32c@gmail.com>
2026-02-22 09:54:30 +0500

    update today class code

---
+++ 03. JAVASCRIPT/chapter-76-to-End-Js/app.js
@@ -0,0 +1,43 @@
+// Javascript Book es5 End:
+
+
+// function Student(first, last) {
+//     this.firstName = first,
+//     this.lastName = last
+// }
+
+// Student.prototype.fullName = function () {
+//     return `${this.firstName} ${this.lastName}`
+// }
+
+// var std = new Student("Ali", "Raza")
+
+// // console.log(std);
+
+
+// console.log(Object.entries(std));
+
+
+
+
+// var pageOpen = window.open("home.html", );
+// var pageOpen = window.open("home.html", "Window-1", "width=600,height=500,left=800,top=100");
+
+
+// function closeWindow() {
+//     pageOpen.close()
+// }
+
+
+// Regex: 
+
+// /^\d{5}(?:[-\s]\d{4})?$/.test("35644")
+// true
+// /^[0-9]{5}-[0-9]{7}-[0-9]$/.test("42201-9660489-3")
+// true
+// /^[0-9]{5}-[0-9]{7}-[0-9]$/.test("4220196604893")
+// false
+// /^\d{5}(?:[-\s]\d{4})?$/.test("356445")
+// false
+// /^\d{5}(?:[-\s]\d{4})?$/.test("3565")
+// false
