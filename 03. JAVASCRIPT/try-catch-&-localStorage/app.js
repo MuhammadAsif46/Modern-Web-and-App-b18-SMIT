91dd7440e050feb7054d2c58916bf1fbc5d04e3a
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-02-27 09:36:38 +0500

    today local storage class

---
+++ 03. JAVASCRIPT/try-catch-&-localStorage/app.js
@@ -0,0 +1,28 @@
+// Javascript Book es5 End:
+
+// try {
+//     console.log("hello");
+//     console.lo("world")
+// } catch (error){
+//     console.log(error);
+//     // throw "Something went wrong!"
+// }
+
+
+// alert("one")
+
+
+
+ 
+// localStorage.setItem("name", "Farhan Ahmed")
+// localStorage.setItem("email", "farhanahmed@gmail.com")
+// localStorage.setItem("grade", "10th")
+
+
+// var getValue = localStorage.getItem("name")
+// console.log(getValue);
+
+// localStorage.removeItem("email")
+// localStorage.removeItem("name")
+
+// localStorage.clear()
