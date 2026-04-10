0ff4fda97c17e3dc917277fe9333585c858715ac
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-10 11:44:37 +0500

    today advance topics class 01

---
+++ 04. ADVANCE-JAVASCRIPT/01. Topics-part-1/app.js
@@ -0,0 +1,77 @@
+// VAR vs LET vs CONST:
+
+// var str = "hello"
+// var str = "world"
+// console.log(str);
+
+
+// var a = "abc"
+
+// function foo(){
+//    var b = "xyz" 
+//    let c = "hello"
+//    const d = "world"
+// }
+    
+    
+// {} Block Scope
+
+// {
+//     let username = "Faraz"
+//     console.log(username); 
+// }
+        
+// console.log(username);
+        
+// ======= VAR VARIABLE ======= //
+
+// var b = "abc"
+// b = "xyz"
+// console.log(b);
+        
+// ======= LET VARIABLE ======= //
+
+// let a = "hello"
+// a = "world"
+// console.log(a);
+
+// ======= CONST VARIABLE ======= //
+
+// const firstname = "Ali"
+// // firstname = "Raza" // cannot update variable
+// console.log(firstname);
+
+// ======= Template Literals ======= //
+
+// let firstName = "Babar"
+// let lastName = "Azam"
+// console.log("Welcome to " + firstName + " " + lastName);
+// console.log(`Welcome to our city! ${firstName} ${lastName}`);
+
+// ======= TERNARY OPERATOR ======= //
+
+// let data = "hello"
+
+// if(data === "hello"){
+//     console.log(`${data} is here!`);
+// } esle{
+//     console.log(`${data} is not here!`);
+// }
+
+
+// let data  = "world"
+// data === "hello" ? console.log(`${data} is here!`) : console.log(`${data} is not here!`);
+
+// let num  = 34;
+// num === 30 ? console.log(++num) : console.log(--num);
+
+// if(data === "hello"){
+//     console.log(`${data} is here!`);
+// } else if (data === "world"){
+//     console.log(`${data} is not here!`);
+// } else {
+//     console.log("kuch bhi");    
+// }
+
+
+
