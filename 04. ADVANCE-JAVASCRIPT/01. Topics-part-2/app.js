b4a57e60f028b157002afdcbb5cdc5ff6c94245e
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-12 12:13:55 +0500

    today advance topics class 02

---
+++ 04. ADVANCE-JAVASCRIPT/01. Topics-part-2/app.js
@@ -0,0 +1,38 @@
+// var a;
+// Destructuring 		
+// Default parameters - 		
+// Rest parameter 		
+// Spread Operator
+// Hoisting -- cover
+// Arrow function - cover
+
+
+// var a = "world"
+// console.log(a);
+
+
+// function abc(data) {
+//     console.log("hello", data);
+// }
+
+// abc("welcome")
+
+
+// let b = "user"
+
+// const greet = (num) => {
+//     console.log("Arow Function", num);
+// }
+
+// greet(123)
+
+
+
+// const greet = (data = "Welcome") => {
+//     console.log("Arow Function", data);
+// }
+
+// greet("New Feature")
+
+
+"https://api.openweathermap.org/data/2.5/weather?appid={3eb5e90e7b2967bdc06f1f1906da667b}&q=karachi"
