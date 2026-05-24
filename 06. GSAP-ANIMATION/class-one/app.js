2177dda22f586497cd376dcf5fefa0e472c60031
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-24 12:22:16 +0500

    today class gsap basics and task

---
+++ 06. GSAP-ANIMATION/class-one/app.js
@@ -0,0 +1,126 @@
+// basics
+
+// to() // left to right
+// from()  // right toi left
+// fromTo()  // both
+
+// gsap.to(".box1", {
+//     x: 900,
+//     duration: 2,
+//     delay: 1,
+//     // scale: 1.5,
+//     backgroundColor: "orange",
+//     rotate: 360,
+//     borderRadius: "50%"
+// })
+
+// gsap.from(".box2", {
+//     x: 900,
+//     duration: 2,
+//     delay: 1,
+//     // scale: 1.5,
+//     backgroundColor: "yellow",
+//     rotate: 360,
+//     borderRadius: "50%"
+// })
+
+
+// gsap.fromTo(".box1",
+//     {
+//         x: 700,
+//         backgroundColor: "red"
+//     }, // start point
+//     {
+//         x: 0, 
+//         duration: 2, 
+//         backgroundColor: "yellow"
+//     } // end point
+// )
+
+
+
+// gsap.to(".box1", {
+//     y: 500,
+//     duration: 2,
+//     delay: 1,
+//     // scale: 1.5,
+//     backgroundColor: "orange",
+//     rotate: 360,
+//     borderRadius: "50%"
+// })
+
+
+// gsap.to(".box1", {
+//     x: 900,
+//     duration: 2,
+//     delay: 1,
+//     // scale: 1.5,
+//     backgroundColor: "orange",
+//     rotate: 360,
+//     borderRadius: "50%",
+//     repeat: -1,
+//     yoyo: true
+// })
+
+// gsap.from(".box2", {
+//     x: 900,
+//     duration: 2,
+//     delay: 1,
+//     // scale: 1.5,
+//     backgroundColor: "yellow",
+//     rotate: 360,
+//     borderRadius: "50%",
+//     repeat: -1,
+//     yoyo: true
+// })
+
+
+
+// gsap.from(".logo", {
+//     y: -100,
+//     duration: 2
+// })
+
+// gsap.from(".menu", {
+//     y: -100,
+//     duration: 2,
+//     delay: 1
+// })
+
+// gsap.from("li", {
+//     y: -100,
+//     duration: 1,
+//     delay: 1,
+//     stagger: 0.5
+// })
+
+// gsap.from(".btn", {
+//     y: -100,
+//     duration: 2,
+//     delay: 3
+// })
+
+// gsap.from("h1", {
+//     // y: -900,
+//     x: -1800,
+//     duration: 3,
+//     delay: 4,
+//     // color: "aqua"
+// })
+
+
+
+// gsap.from("li", {
+//     y: -300,
+//     duration: 2,
+//     delay: 1,
+//     stagger: 1
+// })
+
+
+// gsap.to(".football", {
+//     y: -600,
+//     duration: 1,
+//     repeat: -1,
+//     yoyo: true
+// })
