5d68058dabdcaa373c79a4b50f9c4735941790a6
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-31 11:13:41 +0500

    today typescript class part 01

---
+++ 07. Typescript/1. Class-one/app.js
@@ -0,0 +1,32 @@
+"use strict";
+// ======= Type Annotations ========= //
+// let username:string = "Jhon"
+// username = "Alia"
+// console.log(username);
+// let rollNo:number = 321
+// rollNo = 123
+// console.log(rollNo);
+// let isActive:boolean = true
+// isActive = false
+// console.log(isActive);
+// let cities: string[] = ["khi", "lhr"]
+// console.log(cities);
+// function sum(a: number, b: number):number {
+//     return a + b
+// }
+// console.log(sum(2, 12));
+// const sum = (a: number, b: number):number => {
+//     return a + b
+// }
+// console.log(sum(2, 12));
+// const greet = (stdname:string, city:string):string => {
+//     return `Hello, ${stdname} from ${city}`
+// }
+// console.log(greet("Jhon", "Karachi"));
+// ======= Type Inference ========= //
+let teacherName = "Ali baba";
+teacherName = "Jhon";
+console.log(teacherName);
+let age = 32;
+age = 16;
+console.log(age);
