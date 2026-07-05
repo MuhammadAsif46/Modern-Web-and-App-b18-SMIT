0a5f75d837e8ab9312139641a4d345285b2e54e7
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-07-05 12:23:36 +0500

    firebase class students practice code

---
+++ 08. Firebase/07. class-practice/app.js
@@ -0,0 +1,102 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth,createUserWithEmailAndPassword , signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js"
+import { getFirestore,doc, setDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAtyYX7DKWnUM3OyeCXiwsTY3dLeJEgzho",
+    authDomain: "smit-b18-class.firebaseapp.com",
+    databaseURL: "https://smit-b18-class-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smit-b18-class",
+    storageBucket: "smit-b18-class.firebasestorage.app",
+    messagingSenderId: "322917110595",
+    appId: "1:322917110595:web:5d50e03396ec0d35fe15f6",
+    measurementId: "G-WR83BLHLPY"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+// // Signup form
+
+// let userName = document.querySelector('#UserName');
+// let email = document.querySelector('#email');
+// let password = document.querySelector('#password');
+// let phone = document.querySelector('#phone');
+// let btn = document.querySelector('button')
+
+// // let loginBtn = document.getElementById("loginBtn")
+
+// btn.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     // console.log(userName.value,password.value,email.value,phone.value);
+//  createUserWithEmailAndPassword(auth, email.value, password.value)
+//   .then(async(userCredential) => {
+//     // Signed up 
+//     const user =   userCredential.user;
+//     let userId = user.uid;
+
+//     console.log("signup");
+
+// await setDoc(doc(db, "students", userId), {
+//  email: email.value,
+//  password:password.value,
+//  userName:userName.value,
+//  phone:phone.value
+// });
+
+// console.log("data add sucessfully")
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+    
+//     // ..
+//   });
+
+
+
+    
+
+// Login Form
+
+// let userName = document.querySelector ('#UserName');
+// let password = document.querySelector ('#password');
+// let button = document.querySelector('#loginBtn')
+
+// button.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     console.log(userName,password);
+    
+// })
+// // const email=document.getElementById("email").value;
+// const password=document.getElementById("password').value;
+
+const loginEmail = document.getElementById("loginEmail")
+const loginPassword = document.getElementById("loginPassword")
+const loginBtn = document.getElementById("loginBtn")
+
+
+loginBtn.addEventListener("click" , async () => 
+{
+
+    try {
+    
+
+        const userCredential = await signInWithEmailAndPassword(auth , loginEmail.value ,loginPassword.value)
+        
+        console.log("login success")
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+
+
+
+
+
